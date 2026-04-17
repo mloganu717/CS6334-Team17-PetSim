05598f2940b09cab96d8e5af09e8f4535cc93f90
Logan Margabandu <sugarglider717@gmail.com>
2026-04-17 15:01:17 -0500

    Fixed errors regarding TV Prefab and null mesh

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -219,7 +219,7 @@ public class Outline : MonoBehaviour {
   }
 
   List<Vector3> SmoothNormals(Mesh mesh) {
-
+    if (mesh == null) return new List<Vector3>();
     // Group vertices by location
     var groups = mesh.vertices.Select((vertex, index) => new KeyValuePair<Vector3, int>(vertex, index)).GroupBy(pair => pair.Key);
 
